fcc7e85f630602b7de0f5d5dfcb68c8002cd2d84
xithalius <28624396+xithalius@users.noreply.github.com>
2023-12-30 13:40:28 +0100

    clean-up placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-playground';
+  protected title = 'Miaauw';
 }
